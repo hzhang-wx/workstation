7452c0f520467de9d1174677629d294f2a5a1a1e
Huan Zhang <huazhang@redhat.com>
2013-09-03 17:31:39 +0800

    add install compton to auto.sh && add vim map for <C-x>

--- config/.vimrc
+++ config/.vimrc
@@ -129,6 +129,7 @@ noremap <C-h> <C-W>h
 noremap <C-j> <C-W>j
 noremap <C-k> <C-W>k
 noremap <C-l> <C-W>l
+noremap <C-x> <C-W>x
 noremap <C-W> <C-W><C-W>
 
 " Line completion
--- config/auto.sh
+++ config/auto.sh
@@ -19,9 +19,10 @@ git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 yum install -y feh 
 yum install -y cairo
 yum install -y awesome
+yum install -y compton
 cp -rf config/awesome ~/.config/
 chmod 755 ~/.xinitrc
 sudo ln -s ~/.xinitrc /usr/bin/awesome-xinitrc
 sudo sed -i 's/awesome/awesome-xinitrc/g'
 
-echo "Please don't forget to Install Bundle"cairo
+echo "Please don't forget to Install Bundle"
